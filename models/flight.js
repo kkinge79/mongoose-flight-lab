778f99035ca64ea59b02fc037d329e84d9a1bd84
kkinge79 <kingerykagan@gmail.com>
2021-10-28 22:28:24 -0500

    added nav bar with minimal css

--- models/flight.js
+++ models/flight.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
   airline: String,
-  airport: String,
+  airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: "DEN"},
   flightNo: Number,
   departs: Date,
 })
