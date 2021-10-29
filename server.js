aaddcd77f5bd8cd2209a6a075fb854b57255682a
kkinge79 <kingerykagan@gmail.com>
2021-10-28 20:28:05 -0500

    displaying add flight form

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import "dotenv/config.js"
+
+
+
+import('./config/database.js')
+
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as flightsRouter } from './routes/flights.js'
 
 // set up app
 const app = express()
@@ -32,7 +38,7 @@ app.use(
 
 // mounted routers
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/flights', flightsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
