0e352b77d829d4f4acd482b26c19d15ab332dc50
kkinge79 <kingerykagan@gmail.com>
2021-11-01 14:22:46 -0500

    added destination router,contr, and model

---
+++ models/destination.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const destinationSchema = new Schema({
+  destination: {type: String, required: true, unique: true},
+}, {
+  timestamps: true
+})
+
+const Destination = mongoose.model('Destination', destinationSchema)
+
+export {
+  Destination
+}
